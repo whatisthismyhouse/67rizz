18a2af55b2b9cf6a353cf046b4052c82c4745a0e
whatisthismyhouse <nicl82@duck.com>
2026-08-31 20:13:14 +0930

    this code is awful

--- i dont know how to name files.js
+++ i dont know how to name files.js
@@ -11,7 +11,89 @@ let musicPlaying = false;
 
 function swapYipee(event) {
     if (scratch.hidden) {
-        yipeeButton.textContent = "No more";
+        yipeeButton.textContent = "No more";aconst wikiButton = document.getElementById("wikipedia");
+const primary67 = document.getElementById("primary67");
+const tlotog = document.getElementById("tlotog");
+const musicbutton = document.getElementById("musictoggle");
+const contactpopup = document.getElementById("contact");
+const tabascopopup = document.getElementById("tabascojokes")
+const wikipopup = document.getElementById("wiki")
+
+let numberVal = 0;
+let musicPlaying = false;
+
+function incrementNum() {
+    if (numberVal == 6) {
+        numberVal = 6.7;
+        primary67.textContent = numberVal;
+        document.getElementById("bottombar").classList.toggle("show");
+    }
+    if (numberVal == 6.7) {
+        return;
+    }
+    numberVal++;
+    primary67.textContent = numberVal;
+}
+
+primary67.addEventListener("click", incrementNum);
+
+function togglemusic() {
+    if (!musicPlaying) {
+        tlotog.play();
+        musicbutton.textContent = "MAKE IT STOP";
+        musicPlaying = true;
+    } else {
+        tlotog.pause();
+        tlotog.currentTime = 0;
+        musicbutton.textContent = "Music";
+        musicPlaying = false;
+    }
+}
+
+musicbutton.onclick = togglemusic;
+tlotog.addEventListener("ended", () => {
+    musicbutton.textContent = "Music";
+    musicPlaying = false;
+})
+
+function togglecontact() {
+    if (!contactpopup.open) {
+        contactpopup.showModal();
+    } else {
+        contactpopup.close();
+    }
+}
+
+document.getElementById("contactbutton").onclick = togglecontact;
+document.getElementById("contactleave").onclick = togglecontact;
+
+function contribute() {
+    window.open("https://github.com/whatisthismyhouse/67rizz");
+}
+
+document.getElementById("contr").onclick = contribute;
+
+function toggletabasco() {
+    if (!tabascopopup.open) {
+        tabascopopup.showModal();
+    } else {
+        tabascopopup.close();
+    }
+}
+
+function togglewiki() {
+    if (!wikipopup.open) {
+        wikipopup.showModal();
+    } else {
+        wikipopup.close();
+    }
+}
+
+document.getElementById("tabasco").onclick = toggletabasco;
+document.getElementById("tabascoleave").onclick = toggletabasco;
+document.getElementById("wikipedia").onclick = togglewiki;
+document.getElementById("wikileave").onclick = togglewiki;
+
         arcadepopup.showModal();
     } else {
         yipeeButton.textContent = "Arcade";
